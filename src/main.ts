3c6cd17d7809987eb0cee75a7bd6b08219e0358f
okovtkov <okovtkov@gmail.com>
2024-02-07 21:31:14 +0100

    feat: setup vuetify

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 import './assets/main.css'
+import 'vuetify/styles'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+const app = createApp(App);
+const vuetify = createVuetify({
+  components,
+  directives,
+})
 
-const app = createApp(App)
-
-app.use(router)
-app.mount('#app')
+app.use(vuetify);
+app.use(router);
+app.mount('#app');
