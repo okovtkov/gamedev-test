ae45fbc1bb1b27318cb859ccf8fae5cd9bdcc211
okovtkov <okovtkov@gmail.com>
2024-02-08 01:54:31 +0100

    feat: create markup

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,63 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-</script>
-
 <template>
-  <header>
-    <nav>
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/about">About</RouterLink>
-    </nav>
-  </header>
-
-  <RouterView />
+  <v-app>
+    <v-toolbar class="toolbar bg-cyan" dense floating>
+      <v-text-field hide-details density="compact" />
+      <v-btn class="search-btn" icon="mdi-magnify" />
+    </v-toolbar>
+    <v-data-table :items="items" />
+    <v-footer app border>
+      Directed by
+      <a class="gh-link" href="https://github.com/okovtkov">Oleg Kovtkov</a>
+    </v-footer>
+    <v-dialog v-model="dialog" width="auto">
+      <v-card>
+        <v-card-text>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua.
+        </v-card-text>
+        <v-card-actions>
+          <v-btn color="primary" block @click="dialog = false">
+            Close Dialog
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-app>
 </template>
 
-<style>
+<script lang="ts">
+export default {
+  name: 'App',
+  components: {},
 
+  data() {
+    return {
+      dialog: false,
+      items: [
+        {
+          name: 'African Elephant',
+          species: 'Loxodonta africana',
+          diet: 'Herbivore',
+          habitat: 'Savanna, Forests',
+        },
+      ],
+    };
+  },
+}
+</script>
+
+<style>
+.toolbar {
+  padding-left: 10px;
+}
+
+.search-btn {
+  margin-left: 10px;
+}
+
+.gh-link {
+  margin-left: 6px;
+  color: rgb(0, 188, 212);
+  text-decoration: none;
+}
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,17 @@
-import './assets/main.css'
-import 'vuetify/styles'
+import './assets/base.scss';
+import 'vuetify/styles';
+import '@mdi/font/css/materialdesignicons.css';
 import { createApp } from 'vue';
 import App from './App.vue';
-import router from './router';
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
 
 const app = createApp(App);
 const vuetify = createVuetify({
   components,
   directives,
-})
+});
 
 app.use(vuetify);
-app.use(router);
 app.mount('#app');
