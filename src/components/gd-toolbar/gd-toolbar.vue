af03224df6091a10a980b9ade09738426ed1d45f
okovtkov <okovtkov@gmail.com>
2024-02-09 04:14:46 +0100

    feat: create gd-toolbar component

---
+++ src/components/gd-toolbar/gd-toolbar.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-toolbar class="toolbar bg-cyan" dense floating>
+    <v-text-field
+      placeholder="bootstrap"
+      hide-details
+      density="compact"
+      @keydown.enter="$emit('search', value)"
+      @update:model-value="value = $event"
+    />
+    <v-btn class="toolbar__search-btn" icon="mdi-magnify" @click="$emit('search', value)" />
+  </v-toolbar>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'GdToolbar',
+  data() {
+    return {
+      value: '',
+    };
+  },
+}
+</script>
+
+<style>
+.toolbar {
+  padding-left: 10px;
+}
+
+.toolbar__search-btn {
+  margin-left: 10px;
+}
+</style>
