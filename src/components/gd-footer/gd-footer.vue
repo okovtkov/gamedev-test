0ab2b0a2ea572e080b415a2e4d6d41c619d8cbcf
okovtkov <okovtkov@gmail.com>
2024-02-09 04:14:06 +0100

    feat: create footer component

---
+++ src/components/gd-footer/gd-footer.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-footer border :height="40" class="footer" style="flex: none;">
+    <div class="footer__wrapper">
+      <div class="footer__gh-wrapper">
+        <span>Directed by</span>
+        <a class="footer__link" target="_blank" href="https://github.com/okovtkov">Oleg Kovtkov</a>
+      </div>
+      <div class="footer__tg-wrapper">
+        <p>telegram: <b class="footer__tg">okovtkov</b></p>
+      </div>
+    </div>
+  </v-footer>
+</template>
+
+<style lang="scss">
+.footer {
+  margin-top: auto;
+}
+
+.footer__wrapper {
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+}
+
+.footer__link {
+  margin-left: 6px;
+  color: rgb(0, 188, 212);
+  text-decoration: none;
+}
+
+.footer__tg-wrapper {
+  @media screen and (max-width: 375px) {
+    display: none;
+  }
+}
+
+.footer__tg {
+  color: rgb(0, 188, 212);
+}
+</style>
